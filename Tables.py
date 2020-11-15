1ab664f79103c34c6a6a25569cec4738f722bb73
Mohammad Alam <74500730+MohammadAlam-Genius@users.noreply.github.com>
2020-11-15 22:20:58 +0530

    Updated

--- Tables.py
+++ Tables.py
@@ -1,3 +1,25 @@
 print("Didn't you learnt Tables in your childhood?")
+
 print("Don't worry, I will solve your problem !!")
-ta = int(input("Enter the number")
+
+ta = int(input("Enter the number: "))
+
+print(ta*1)
+
+print(ta*2)
+
+print(ta*3)
+
+print(ta*4)
+
+print(ta*5)
+
+print(ta*6)
+
+print(ta*7)
+
+print(ta*8)
+
+print(ta*9)
+
+print(ta*10
